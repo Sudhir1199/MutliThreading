a422bb0880f2cd8eceba671f4ced3813e224b436
Sudhir <60312216+Sudhir1199@users.noreply.github.com>
2023-04-19 11:38:37 +0530

    Add files via upload

---
+++ Cases_ofThreads/Case7.java
@@ -0,0 +1,29 @@
+
+class MyThread extends Thread{
+	@Override
+	public  void start() {
+		// TODO Auto-generated method stub
+		super.start();
+		System.out.println("Start Method");
+	}
+	
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		System.out.println("run method");
+	}
+
+}
+public class Case7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MyThread t = new MyThread();
+		t.start();
+		System.out.println("main method");
+		
+
+		
+	}
+
+}
