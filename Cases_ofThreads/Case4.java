5a075e10228eb487d157b07c08eefd3dbcee600f
Sudhir <60312216+Sudhir1199@users.noreply.github.com>
2023-04-19 06:18:49 +0530

    Add files via upload
    If we are not overriding run() method then Thread class run() method will be executed which has empty implementation and hence we won't get any output

---
+++ Cases_ofThreads/Case4.java
@@ -0,0 +1,14 @@
+
+class MyThread extends Thread{
+	
+}
+public class Case4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MyThread t = new MyThread();
+		t.start();
+
+	}
+
+}
