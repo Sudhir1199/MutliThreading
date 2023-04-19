2db11d745f521e92288aa65ad51a540a565a3f19
Sudhir <60312216+Sudhir1199@users.noreply.github.com>
2023-04-19 11:00:54 +0530

    Update Case6.java

--- Cases_ofThreads/Case6.java
+++ Cases_ofThreads/Case6.java
@@ -1,3 +1,11 @@
+// When we call start() method on thread, it internally calls run() method by default with 
+//newly created thread. SO, if we override start() method, run() method will not called
+//until we write code for run() method.
+
+//  so if we override start() method  then out start() method will be executed just like a normal
+//method call without creating any new Thread.
+
+//**which is entire program will produce by single main Thread
 
 class MyThread extends Thread{
 	@Override
